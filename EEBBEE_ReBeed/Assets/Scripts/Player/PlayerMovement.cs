2a7542a771e235823883f8725fbf1f895e714148
HalHarris1501 <halharris1501@gmail.com>
2024-07-01 15:08:07 +0100

    Added in force based player movement
    With max speed clamp

--- EEBBEE_ReBeed/Assets/Scripts/Player/PlayerMovement.cs
+++ EEBBEE_ReBeed/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _beeRigidBody;
+    private PlayerInputActions playerInputActions;
+    [SerializeField] private float _acceleration = 5f;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _maxSpeed;
+
+    private void Awake()
+    {
+        _beeRigidBody = GetComponent<Rigidbody2D>();
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Keyboard.Enable();
+    }
+
+    //movebee
+    private void Movement()
+    {
+        Vector2 inputVector = playerInputActions.Keyboard.Movement.ReadValue<Vector2>();
+        _beeRigidBody.AddForce(new Vector3(inputVector.x, inputVector.y, 0) * _acceleration, ForceMode2D.Force);
+        if (_beeRigidBody.velocity.magnitude > _maxSpeed)
+        {
+            _beeRigidBody.velocity = Vector2.ClampMagnitude(_beeRigidBody.velocity, _maxSpeed);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +40,11 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        _speed = _beeRigidBody.velocity.magnitude;
+        Movement();
     }
 }
