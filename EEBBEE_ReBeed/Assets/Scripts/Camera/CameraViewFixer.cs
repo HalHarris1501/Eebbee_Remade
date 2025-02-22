29806677714db13259a68c268878453422571fc8
HalHarris1501 <halharris1501@gmail.com>
2025-02-22 20:54:59 +0000

    Attempted better solution
    It was worse

--- EEBBEE_ReBeed/Assets/Scripts/Camera/CameraViewFixer.cs
+++ EEBBEE_ReBeed/Assets/Scripts/Camera/CameraViewFixer.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class CameraViewFixer : MonoBehaviour
 {
-    [SerializeField] private float orthographicSize = 5;
-    [SerializeField] private float aspect = 1.33333f;
+    [Header("The Camera")]
+    [SerializeField] private Camera _cam;
+
+    [SerializeField] private float _orthographicSize = 5;
+    [SerializeField] private float _aspect = 1.33333f;
+    //[SerializeField] private List<Collider2D> _objectsToEncapsulate;
+    //[SerializeField] private float _buffer;
 
     void Start()
     {
         Camera.main.projectionMatrix = Matrix4x4.Ortho(
-                -orthographicSize * aspect, orthographicSize * aspect,
-                -orthographicSize, orthographicSize,
+                -_orthographicSize * _aspect, _orthographicSize * _aspect,
+                -_orthographicSize, _orthographicSize,
                 Camera.main.nearClipPlane, Camera.main.farClipPlane);
+
+        //var (centre, size) = CalculateOrthoSize();
+        //_cam.transform.position = centre;
+        //_cam.orthographicSize = size;
     }
+
+    //private (Vector3 centre, float size) CalculateOrthoSize()
+    //{
+    //    var bounds = new Bounds();
+
+    //    foreach (var col in _objectsToEncapsulate) bounds.Encapsulate(col.bounds);
+
+    //    bounds.Expand(_buffer);
+
+    //    var vertical = bounds.size.y;
+    //    var horizontal = bounds.size.x * _cam.pixelHeight / _cam.pixelWidth;
+
+    //    var size = Mathf.Max(horizontal, vertical) * 0.5f;
+    //    var centre = bounds.center + new Vector3(0, 0, -1);
+
+    //    return (centre, size);
+    //}
 }
