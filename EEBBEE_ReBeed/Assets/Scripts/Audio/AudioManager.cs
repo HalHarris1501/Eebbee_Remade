52934e762eeeee9c1541144b376f075916319f19
HalHarris1501 <halharris1501@gmail.com>
2025-03-09 15:01:01 +0000

    Added an audio manager and audio sources with some temp music

---
+++ EEBBEE_ReBeed/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //Singleton pattern
+    #region Singleton
+    private static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get //making sure that a game manager always exists
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+            }
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("AudioManager");
+                _instance = go.AddComponent<AudioManager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioSource _soundEffectsSource;
+
+    [Header("Audio clips")]
+    [SerializeField] private List<AudioStorage> _audioClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayMusic(AudioTag.MenuMusic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySoundAffect(AudioTag tag)
+    {
+        if (FindClip(tag) != null)
+        {
+            _soundEffectsSource.clip = FindClip(tag);
+            _soundEffectsSource.Play();
+        }
+        else
+        {
+            Debug.Log("No sound affect returned");
+        }
+    }
+
+    public void PlayMusic(AudioTag tag)
+    {
+        if (FindClip(tag) != null)
+        {
+            _musicSource.clip = FindClip(tag);
+            _musicSource.Play();
+        }
+        else
+        {
+            Debug.Log("No sound affect returned");
+        }
+    }
+
+    public AudioClip FindClip(AudioTag tag)
+    {
+        AudioClip soundToPlay = null;
+
+        foreach(AudioStorage clip in _audioClips)
+        {
+            if(clip.Tag == tag)
+            {
+                soundToPlay = clip.Clip;
+            }
+        }
+
+        return soundToPlay;
+    }
+}
---
+++ EEBBEE_ReBeed/Assets/Scripts/Audio/AudioStorage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioStorage 
+{
+    public AudioTag Tag;
+    public AudioClip Clip;
+}
+
+public enum AudioTag
+{ 
+    BuzzSound,
+    MenuMusic,
+    GameMusic,
+    DeathSound,
+    WinSound,
+    StartSound,
+}
+
