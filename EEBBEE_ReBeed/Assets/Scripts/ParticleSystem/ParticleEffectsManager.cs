e5c22a9c17441ac50b451fba8922b26c71997493
HalHarris1501 <halharris1501@gmail.com>
2025-05-15 16:04:51 +0100

    Particle systems
    -Added nectar particles system
    --nectar particles increase in amount based on player's current score
    -Started making Death explosion particle

---
+++ EEBBEE_ReBeed/Assets/Scripts/ParticleSystem/ParticleEffectsManager.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectsManager : MonoBehaviour, IObserver<Score>
+{
+    [Header("Variables")]
+    private int _currentScore = 0;
+
+    [Header("ParticleSystemReferences")]
+    [SerializeField] private ParticleSystem _nectarSystem;
+
+    //Singleton pattern
+    #region Singleton
+    private static ParticleEffectsManager _instance;
+    public static ParticleEffectsManager Instance
+    {
+        get //making sure that a Affect manager always exists
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ParticleEffectsManager>();
+            }
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("ParticleEffectsManager");
+                _instance = go.AddComponent<ParticleEffectsManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreManager.Instance.RegisterObserver(this);
+        PlayerMovement.onPlayerDeath += StopNectar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        PlayerMovement.onPlayerDeath -= StopNectar;
+    }
+
+    private void UpNectarCount()
+    {
+        ParticleSystem.EmissionModule nectarSystemEmission = _nectarSystem.emission;
+        nectarSystemEmission.rateOverTime = Mathf.FloorToInt(_currentScore/5);
+    }
+
+    private void StopNectar()
+    {
+        ParticleSystem.EmissionModule nectarSystemEmission = _nectarSystem.emission;
+        nectarSystemEmission.rateOverTime = 0;
+    }
+
+    public void ItemAltered(Score score, int count)
+    {
+        _currentScore = score.ScoreCount;
+        UpNectarCount();
+    }
+
+    public void ItemRemoved(Score type)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void NewItemAdded(Score type)
+    {
+        throw new System.NotImplementedException();
+    }
+}
